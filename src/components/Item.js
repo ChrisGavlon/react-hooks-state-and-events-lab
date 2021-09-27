74813ffcb523d385c772a16e5d5faffae30f1228
Chris <ChrisGavlon@gmail.com>
2021-09-27 02:15:00 -0500

    completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [InOrNot, setInOrNot] = useState(null)
+  function addToCart(){
+   !InOrNot ? setInOrNot("in-cart") : setInOrNot(null);
+  }
+
   return (
-    <li className="">
+    <li className={InOrNot}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addToCart}>{!InOrNot? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
